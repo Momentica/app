43558062b034f6eba637b7c58a4e577c86516347
유지우 <treeyu12345@naver.com>
2024-11-01 20:19:53 +0900

    FEAT :: 라우터 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:momentica/core/di/momentica_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const MyHomePage(),
+    return MaterialApp.router(
+      routerConfig: momenticaRouter,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
