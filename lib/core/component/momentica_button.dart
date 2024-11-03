0d61d7bc598c1deafc7b2119cebdd3151c6450fa
유지우 <treeyu12345@naver.com>
2024-11-03 23:14:37 +0900

    FEAT :: 버튼 컴포넌트 추가

---
+++ lib/core/component/momentica_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:momentica/core/component/momentica_gesture.dart';
+
+class MomenticaButton extends StatelessWidget {
+  final Function event;
+  final double? height, width, radius;
+  final Color backgroundColor;
+  final Widget content;
+
+  const MomenticaButton({
+    super.key,
+    required this.event,
+    required this.backgroundColor,
+    required this.content,
+    this.height,
+    this.width,
+    this.radius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MomenticaGesture(
+      event: () => event(),
+      child: Container(
+        width: width ?? MediaQuery.of(context).size.width,
+        height: height ?? 52,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(radius ?? 12),
+        ),
+        child: content,
+      ),
+    );
+  }
+}
