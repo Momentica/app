2eedf0864460c41f177bf50f89ba98dee408feaf
유지우 <treeyu12345@naver.com>
2024-11-03 23:13:57 +0900

    FEAT :: Cursor Theme 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:momentica/core/di/momentica_router.dart';
+import 'package:momentica/core/di/momentica_style.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       routerConfig: momenticaRouter,
+      theme: ThemeData(
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: MomenticaColor.main,
+          selectionHandleColor: MomenticaColor.main,
+          selectionColor: MomenticaColor.systemGray200,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
     );
   }
