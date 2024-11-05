ebbfd9fb52c7f964b99a290f6edf9b689ad08255
유지우 <treeyu12345@naver.com>
2024-11-05 15:41:19 +0900

    ENCHANT :: 텍스트 필드 포커스 Provider 적용 및 SuffixIcon 적용

--- lib/core/component/momentica_text_field.dart
+++ lib/core/component/momentica_text_field.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:momentica/core/component/momentica_button.dart';
 import 'package:momentica/core/component/momentica_gesture.dart';
-import 'package:momentica/core/di/momentica_asset.dart';
 import 'package:momentica/core/di/momentica_style.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:momentica/core/provider/text_field_focus_provider.dart';
 import 'package:momentica/core/type/suffix_type.dart';
 
-class MomenticaTextField extends StatefulWidget {
+class MomenticaTextField extends ConsumerStatefulWidget {
   final TextEditingController controller;
 
   final FocusNode focusNode;
 
   final String caption;
 
-  final String? hintText;
+  final String? hintText, buttonContent;
 
   final bool? autofocus;
 
   final Widget? suffixIcon;
+
+  final Function? buttonEvent;
 
   final int? maxLength;
 
@@ -37,22 +41,25 @@ class MomenticaTextField extends StatefulWidget {
     required this.type,
     this.hintText,
     this.autofocus = false,
+    this.buttonContent,
+    this.buttonEvent,
     this.suffixIcon,
     this.maxLength,
-    this.textInputAction,
-    this.textInputType,
+    this.textInputAction = TextInputAction.next,
+    this.textInputType = TextInputType.text,
     this.textInputFormatter,
   });
 
   @override
-  State<MomenticaTextField> createState() => _MomenticaTextFieldState();
+  ConsumerState<MomenticaTextField> createState() => _MomenticaTextFieldState();
 }
 
-class _MomenticaTextFieldState extends State<MomenticaTextField> {
+class _MomenticaTextFieldState extends ConsumerState<MomenticaTextField> {
   bool _isClicked = false;
 
   @override
   Widget build(BuildContext context) {
+    final updateFocus = ref.read(textFieldFocusProvider.notifier);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -63,96 +70,141 @@ class _MomenticaTextFieldState extends State<MomenticaTextField> {
           ),
         ),
         const SizedBox(height: 8),
-        TextField(
-          controller: widget.controller,
-          focusNode: widget.focusNode,
-          style: MomenticaTextStyle.caption1(
-            color: MomenticaColor.black,
-          ),
-
-          autofocus: widget.autofocus!,
-
-          maxLength: widget.maxLength,
-
-          obscureText:
-              widget.type == SuffixType.password && !_isClicked ? true : false,
-
-          cursorWidth: 1,
-
-          decoration: InputDecoration(
-            hintText: widget.hintText ?? "placeholder",
-            hintStyle: MomenticaTextStyle.caption1(
-              color: MomenticaColor.systemGray300,
-            ),
-
-            filled: true,
-            fillColor: MomenticaColor.systemGray50,
-
-            contentPadding: const EdgeInsets.symmetric(horizontal: 12),
-
-            border: const OutlineInputBorder(
-              borderSide: BorderSide.none,
-            ),
-
-            suffixIcon: switch (widget.type) {
-              SuffixType.none => null,
-              SuffixType.eraser => MomenticaGesture(
-                  event: () => widget.controller.clear(),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                    child: SvgPicture.asset(
-                      "$iconCoreAsset/circle_close_icon.svg",
-                    ),
-                  ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Expanded(
+              child: TextField(
+                controller: widget.controller,
+                focusNode: widget.focusNode,
+                style: MomenticaTextStyle.caption1(
+                  color: MomenticaColor.black,
                 ),
-              SuffixType.password => MomenticaGesture(
-                  event: () {
-                    setState(() {
-                      _isClicked = !_isClicked;
-                    });
+
+                autofocus: widget.autofocus!,
+
+                inputFormatters: widget.textInputFormatter != null
+                    ? [widget.textInputFormatter!]
+                    : [],
+
+                maxLength: widget.maxLength,
+
+                obscureText: widget.type == SuffixType.password && !_isClicked
+                    ? true
+                    : false,
+
+                cursorWidth: 1,
+
+                keyboardType: widget.textInputType,
+
+                textInputAction: widget.textInputAction,
+
+                decoration: InputDecoration(
+                  hintText: widget.hintText ?? "placeholder",
+                  hintStyle: MomenticaTextStyle.caption1(
+                    color: MomenticaColor.systemGray300,
+                  ),
+
+
+                  filled: true,
+                  fillColor: MomenticaColor.systemGray50,
+
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+
+                  suffixIcon: switch (widget.type) {
+                    SuffixType.none => null,
+                    SuffixType.eraser => MomenticaGesture(
+                        event: () => widget.controller.clear(),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                          child: SvgPicture.asset(
+                            "$iconCoreAsset/circle_close_icon.svg",
+                          ),
+                        ),
+                      ),
+                    SuffixType.password => MomenticaGesture(
+                        event: () {
+                          setState(() {
+                            _isClicked = !_isClicked;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                          child: SvgPicture.asset(
+                            "$iconCoreAsset/eyes_${_isClicked ? "open" : "close"}_icon.svg",
+                          ),
+                        ),
+                      ),
+                    SuffixType.custom => widget.suffixIcon,
                   },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                    child: SvgPicture.asset(
-                      "$iconCoreAsset/eyes_${_isClicked ? "open" : "close"}_icon.svg",
+
+                  /// 텍스트 필드가 포커스 되었을 때
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                      color: MomenticaColor.main,
+                      strokeAlign: BorderSide.strokeAlignInside,
+                      width: 1,
+                    ),
+                  ),
+
+                  /// 텍스트 필드가 포커스 되지 않았을 때
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                      color: MomenticaColor.systemGray50,
+                      strokeAlign: BorderSide.strokeAlignInside,
+                      width: 1,
                     ),
                   ),
                 ),
-              SuffixType.custom => widget.suffixIcon,
-            },
 
-            /// 텍스트 필드가 포커스 되었을 때
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(
-                color: MomenticaColor.main,
-                strokeAlign: BorderSide.strokeAlignInside,
-                width: 1,
+                onChanged: (value) {
+                  setState(() {});
+                },
+
+                onTap: () {
+                  if (FocusScope.of(context).hasFocus) {
+                    updateFocus.state = false;
+                    FocusScope.of(context).unfocus();
+                  } else {
+                    updateFocus.state = true;
+                    FocusScope.of(context).hasFocus;
+                  }
+                },
+
+                /// 바깥 영역을 클릭 시 포커스 해제
+                onTapOutside: (event) {
+                  updateFocus.state = false;
+                  FocusScope.of(context).unfocus();
+                },
               ),
             ),
-
-            /// 텍스트 필드가 포커스 되지 않았을 때
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
-              borderSide: const BorderSide(
-                color: MomenticaColor.systemGray50,
-                strokeAlign: BorderSide.strokeAlignInside,
-                width: 1,
+            if (widget.buttonContent != null && widget.buttonEvent != null)
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: MomenticaButton(
+                  width: 80,
+                  height: 48,
+                  event: () => widget.buttonEvent!(),
+                  backgroundColor: widget.controller.text.isEmpty
+                      ? MomenticaColor.systemGray100
+                      : MomenticaColor.main,
+                  content: Text(
+                    widget.buttonContent!,
+                    style: MomenticaTextStyle.button2(
+                      color: widget.controller.text.isEmpty
+                          ? MomenticaColor.systemGray400
+                          : MomenticaColor.white,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-
-          onTap: () {
-            /// 텍스트 필드가 포커스 된 상태에서 텍스트 필드를 클릭하면 포커스 해제
-            FocusScope.of(context).hasFocus
-                ? FocusScope.of(context).unfocus()
-                : FocusScope.of(context).hasFocus;
-          },
-
-          /// 바깥 영역을 클릭 시 포커스 해제
-          onTapOutside: (event) {
-            FocusScope.of(context).unfocus();
-          },
+          ],
         ),
       ],
     );
