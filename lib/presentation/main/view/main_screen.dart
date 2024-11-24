a09bbfd2d00344cbee7c2b212143066900cba3ee
유지우 <treeyu12345@naver.com>
2024-11-25 08:57:34 +0900

    FEAT :: 메인 스크린 추가

---
+++ lib/presentation/main/view/main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:momentica/core/layout/momentica_layout.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MomenticaLayout(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
