d6865dc03a78bc34a1c0d33b5aa78825a33e9859
유지우 <treeyu12345@naver.com>
2024-11-25 08:33:56 +0900

    ADD :: 온보딩 Asset 설정

--- lib/core/di/momentica_asset.dart
+++ lib/core/di/momentica_asset.dart
@@ -1,3 +1,4 @@
 const String iconCoreAsset = "assets/icon/core";
 const String iconAuthAsset = "assets/icon/auth";
 const String imageCoreAsset = "assets/image/core";
+const String imageOnBoardingAsset = "assets/image/on_boarding";
