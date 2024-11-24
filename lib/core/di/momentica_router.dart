a37b84af0fecfa97e761f01fb83d709cbaafeca9
유지우 <treeyu12345@naver.com>
2024-11-25 08:57:44 +0900

    ADD :: 메인 스크린 라우트 설정

--- lib/core/di/momentica_router.dart
+++ lib/core/di/momentica_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:momentica/presentation/main/view/main_screen.dart';
 import 'package:momentica/presentation/on_boarding/view/on_boarding_screen.dart';
 import 'package:momentica/presentation/password/view/password_reset_screen.dart';
 import 'package:momentica/presentation/sign_in/view/sign_in_screen.dart';
@@ -9,18 +10,23 @@ import 'package:momentica/presentation/splash/view/splash_screen.dart';
 final GoRouter momenticaRouter = GoRouter(
   initialLocation: "/splash",
   routes: [
+    /// Splash
     GoRoute(
       path: "/splash",
       builder: (context, state) {
         return const SplashScreen();
       },
     ),
+
+    /// OnBoarding
     GoRoute(
       path: "/onBoarding",
       builder: (context, state) {
         return const OnBoardingScreen();
       },
     ),
+
+    /// Auth
     GoRoute(
       path: "/signIn",
       builder: (context, state) {
@@ -43,6 +49,14 @@ final GoRouter momenticaRouter = GoRouter(
       path: "/password",
       builder: (context, state) {
         return const PasswordResetScreen();
+      },
+    ),
+
+    /// Main
+    GoRoute(
+      path: "/main",
+      builder: (context, state) {
+        return const MainScreen();
       },
     ),
   ],
