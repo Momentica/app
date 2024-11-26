4c9dc404a2b6cdeae65bee80178d3f2902916446
유지우 <treeyu12345@naver.com>
2024-11-26 16:17:37 +0900

    ADD :: Asset 경로 설정

--- lib/core/di/momentica_asset.dart
+++ lib/core/di/momentica_asset.dart
@@ -1,4 +1,5 @@
 const String iconCoreAsset = "assets/icon/core";
 const String iconAuthAsset = "assets/icon/auth";
+const String iconMyAsset = "assets/icon/my";
 const String imageCoreAsset = "assets/image/core";
 const String imageOnBoardingAsset = "assets/image/on_boarding";
