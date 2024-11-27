192be07349d34c285310296af939a9aca4e7593a
유지우 <treeyu12345@naver.com>
2024-11-27 13:44:43 +0900

    FEAT :: 마이페이지 타입 구현

---
+++ lib/core/type/my/my_type.dart
@@ -0,0 +1,35 @@
+import 'package:momentica/core/di/momentica_style.dart';
+
+enum MyType {
+  user(
+    icon: "$iconMyAsset/user_icon.svg",
+    caption: "내 정보",
+  ),
+  address(
+    icon: "$iconMyAsset/home_icon.svg",
+    caption: "배송지",
+  ),
+  order(
+    icon: "$iconMyAsset/widget_icon.svg",
+    caption: "주문 내역",
+  ),
+  question(
+    icon: "$iconMyAsset/question_box_icon.svg",
+    caption: "자주 묻는 질문",
+  ),
+  counselor(
+    icon: "$iconMyAsset/counselor_icon.svg",
+    caption: "고객센터",
+  ),
+  terms(
+    icon: "$iconMyAsset/board_icon.svg",
+    caption: "서비스 이용약관",
+  );
+
+  final String icon, caption;
+
+  const MyType({
+    required this.icon,
+    required this.caption,
+  });
+}
