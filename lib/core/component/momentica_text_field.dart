230cad1c4e217847e829d0285cd6bd226a845a14
유지우 <treeyu12345@naver.com>
2024-11-27 15:18:25 +0900

    ENCHANT :: 다양한 UI 사용을 고려한 텍스트 필드 컴포넌트 수정
    1. 높이, 너비 속성 추가
    2. 제목이 없는 텍스트 필드가 필요할 경우도 고려함
    3. 버튼의 높이와 너비가 상황에 따라 달라지는 경우를 고려함

--- lib/core/component/momentica_text_field.dart
+++ lib/core/component/momentica_text_field.dart
@@ -13,11 +13,13 @@ class MomenticaTextField extends ConsumerStatefulWidget {
 
   final FocusNode focusNode;
 
-  /// 텍스트 필드 타이틀
-  final String caption;
+  /// 높이, 너비, 버튼의 너비
+  final double? width, height, buttonWidth;
 
-  final String? hintText, buttonContent;
+  /// 제목, 힌트, 버튼의 내용
+  final String? caption, hintText, buttonContent;
 
+  /// 자동 포커싱
   final bool? autofocus;
 
   final Widget? suffixIcon;
@@ -25,7 +27,7 @@ class MomenticaTextField extends ConsumerStatefulWidget {
   /// 버튼 이벤트
   final Function? buttonEvent;
 
-  final int? maxLength;
+  final int? maxLength, maxLines;
 
   final TextInputAction? textInputAction;
 
@@ -39,14 +41,18 @@ class MomenticaTextField extends ConsumerStatefulWidget {
     super.key,
     required this.controller,
     required this.focusNode,
-    required this.caption,
     required this.type,
+    this.buttonWidth,
+    this.height,
+    this.width,
+    this.caption,
     this.hintText,
     this.autofocus = false,
     this.buttonContent,
     this.buttonEvent,
     this.suffixIcon,
     this.maxLength,
+    this.maxLines,
     this.textInputAction = TextInputAction.next,
     this.textInputType = TextInputType.text,
     this.textInputFormatter,
@@ -65,135 +71,143 @@ class _MomenticaTextFieldState extends ConsumerState<MomenticaTextField> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          widget.caption,
-          style: MomenticaTextStyle.label1(
-            color: MomenticaColor.black,
+        /// 제목이 없는 경우도 존재함.
+        if (widget.caption != null)
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8),
+          child: Text(
+            widget.caption!,
+            style: MomenticaTextStyle.label1(
+              color: MomenticaColor.black,
+            ),
           ),
         ),
-        const SizedBox(height: 8),
         Row(
           mainAxisSize: MainAxisSize.min,
           children: [
             Expanded(
-              child: TextField(
-                controller: widget.controller,
-                focusNode: widget.focusNode,
-                style: MomenticaTextStyle.caption2(
-                  color: MomenticaColor.black,
-                ),
-
-                autofocus: widget.autofocus!,
-
-                inputFormatters: widget.textInputFormatter != null
-                    ? [widget.textInputFormatter!]
-                    : [],
-
-                maxLength: widget.maxLength,
-
-                obscureText: widget.type == SuffixType.password && !_isClicked
-                    ? true
-                    : false,
-
-                cursorWidth: 1,
-
-                keyboardType: widget.textInputType,
-
-                textInputAction: widget.textInputAction,
-
-                decoration: InputDecoration(
-                  /// placeholder >> 기본값
-                  hintText: widget.hintText ?? "placeholder",
-                  hintStyle: MomenticaTextStyle.caption2(
-                    color: MomenticaColor.systemGray300,
+              child: SizedBox(
+                height: widget.height ?? 48,
+                width: widget.width ?? MediaQuery.of(context).size.width,
+                child: TextField(
+                  controller: widget.controller,
+                  focusNode: widget.focusNode,
+                  style: MomenticaTextStyle.caption2(
+                    color: MomenticaColor.black,
                   ),
 
+                  autofocus: widget.autofocus!,
 
-                  filled: true,
-                  fillColor: MomenticaColor.systemGray50,
+                  inputFormatters: widget.textInputFormatter != null
+                      ? [widget.textInputFormatter!]
+                      : [],
 
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+                  maxLength: widget.maxLength,
 
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide.none,
+                  obscureText: widget.type == SuffixType.password && !_isClicked
+                      ? true
+                      : false,
+
+                  cursorWidth: 1,
+
+                  keyboardType: widget.textInputType,
+
+                  textInputAction: widget.textInputAction,
+
+                  decoration: InputDecoration(
+                    /// placeholder >> 기본값
+                    hintText: widget.hintText ?? "placeholder",
+                    hintStyle: MomenticaTextStyle.caption2(
+                      color: MomenticaColor.systemGray300,
+                    ),
+
+
+                    filled: true,
+                    fillColor: MomenticaColor.systemGray50,
+
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+
+                    border: const OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                    ),
+
+                    /// suffixIcon -> custom 및 다양한 Icon
+                    suffixIcon: switch (widget.type) {
+                      SuffixType.none => null,
+                      SuffixType.eraser => MomenticaGesture(
+                          event: () => widget.controller.clear(),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            child: SvgPicture.asset(
+                              "$iconCoreAsset/circle_close_icon.svg",
+                            ),
+                          ),
+                        ),
+                      SuffixType.password => MomenticaGesture(
+                          event: () {
+                            setState(() {
+                              _isClicked = !_isClicked;
+                            });
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            child: SvgPicture.asset(
+                              "$iconCoreAsset/eyes_${_isClicked ? "open" : "close"}_icon.svg",
+                            ),
+                          ),
+                        ),
+                      SuffixType.custom => widget.suffixIcon,
+                    },
+
+                    /// 텍스트 필드가 포커스 되었을 때
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(
+                        color: MomenticaColor.main,
+                        strokeAlign: BorderSide.strokeAlignInside,
+                        width: 1,
+                      ),
+                    ),
+
+                    /// 텍스트 필드가 포커스 되지 않았을 때
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: const BorderSide(
+                        color: MomenticaColor.systemGray50,
+                        strokeAlign: BorderSide.strokeAlignInside,
+                        width: 1,
+                      ),
+                    ),
                   ),
 
-                  /// suffixIcon -> custom 및 다양한 Icon
-                  suffixIcon: switch (widget.type) {
-                    SuffixType.none => null,
-                    SuffixType.eraser => MomenticaGesture(
-                        event: () => widget.controller.clear(),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          child: SvgPicture.asset(
-                            "$iconCoreAsset/circle_close_icon.svg",
-                          ),
-                        ),
-                      ),
-                    SuffixType.password => MomenticaGesture(
-                        event: () {
-                          setState(() {
-                            _isClicked = !_isClicked;
-                          });
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          child: SvgPicture.asset(
-                            "$iconCoreAsset/eyes_${_isClicked ? "open" : "close"}_icon.svg",
-                          ),
-                        ),
-                      ),
-                    SuffixType.custom => widget.suffixIcon,
+                  onChanged: (value) {
+                    setState(() {});
                   },
 
-                  /// 텍스트 필드가 포커스 되었을 때
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: const BorderSide(
-                      color: MomenticaColor.main,
-                      strokeAlign: BorderSide.strokeAlignInside,
-                      width: 1,
-                    ),
-                  ),
+                  onTap: () {
+                    if (FocusScope.of(context).hasFocus) {
+                      updateFocus.state = false;
+                      FocusScope.of(context).unfocus();
+                    } else {
+                      updateFocus.state = true;
+                      FocusScope.of(context).hasFocus;
+                    }
+                  },
 
-                  /// 텍스트 필드가 포커스 되지 않았을 때
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: const BorderSide(
-                      color: MomenticaColor.systemGray50,
-                      strokeAlign: BorderSide.strokeAlignInside,
-                      width: 1,
-                    ),
-                  ),
-                ),
-
-                onChanged: (value) {
-                  setState(() {});
-                },
-
-                onTap: () {
-                  if (FocusScope.of(context).hasFocus) {
+                  /// 바깥 영역을 클릭 시 포커스 해제
+                  onTapOutside: (event) {
                     updateFocus.state = false;
                     FocusScope.of(context).unfocus();
-                  } else {
-                    updateFocus.state = true;
-                    FocusScope.of(context).hasFocus;
-                  }
-                },
-
-                /// 바깥 영역을 클릭 시 포커스 해제
-                onTapOutside: (event) {
-                  updateFocus.state = false;
-                  FocusScope.of(context).unfocus();
-                },
+                  },
+                ),
               ),
             ),
             if (widget.buttonContent != null && widget.buttonEvent != null)
               Padding(
                 padding: const EdgeInsets.only(left: 8),
                 child: MomenticaButton(
-                  width: 80,
-                  height: 48,
+                  width: widget.buttonWidth ?? 80,
+                  height: widget.height ?? 48,
                   event: () => widget.buttonEvent!(),
                   radius: 12,
                   backgroundColor: widget.controller.text.isEmpty
