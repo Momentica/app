f9fa39426fb45bbf0b199dc978ce17a0693975e5
유지우 <treeyu12345@naver.com>
2024-11-27 13:52:10 +0900

    FEAT :: 마이페이지 비밀번호 변경 페이지 퍼블리싱

---
+++ lib/presentation/my/view/user/my_user_edit_password_screen.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:momentica/core/component/momentica_app_bar.dart';
+import 'package:momentica/core/component/momentica_button.dart';
+import 'package:momentica/core/component/momentica_gesture.dart';
+import 'package:momentica/core/component/momentica_text_field.dart';
+import 'package:momentica/core/di/momentica_style.dart';
+import 'package:momentica/core/layout/momentica_layout.dart';
+import 'package:momentica/core/provider/text_field_focus_provider.dart';
+import 'package:momentica/core/type/suffix_type.dart';
+
+class MyUserEditPasswordScreen extends ConsumerStatefulWidget {
+  const MyUserEditPasswordScreen({super.key});
+
+  @override
+  ConsumerState<MyUserEditPasswordScreen> createState() =>
+      _MyUserEditPasswordScreenState();
+}
+
+class _MyUserEditPasswordScreenState
+    extends ConsumerState<MyUserEditPasswordScreen> {
+  late TextEditingController previousController;
+  late TextEditingController nextController;
+  late TextEditingController confirmController;
+
+  late FocusNode previousFocusNode;
+  late FocusNode nextFocusNode;
+  late FocusNode confirmFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    previousController = TextEditingController();
+    nextController = TextEditingController();
+    confirmController = TextEditingController();
+
+    previousFocusNode = FocusNode();
+    nextFocusNode = FocusNode();
+    confirmFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    previousController.dispose();
+    nextController.dispose();
+    confirmController.dispose();
+
+    previousFocusNode.dispose();
+    nextFocusNode.dispose();
+    nextFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final focusValue = ref.watch(textFieldFocusProvider);
+    return MomenticaLayout(
+      appBar: MomenticaAppBar(
+        height: 56,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 4),
+          child: Row(
+            children: [
+              MomenticaGesture(
+                event: () => context.pop(),
+                child: SvgPicture.asset("$iconCoreAsset/left_arrow_icon.svg"),
+              ),
+              const SizedBox(width: 8),
+              Text(
+                "비밀번호",
+                style: MomenticaTextStyle.caption1(
+                  color: MomenticaColor.black,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Padding(
+          padding: EdgeInsets.only(top: 24, bottom: focusValue ? 0 : 12),
+          child: MomenticaButton(
+            event: () {},
+            height: 52,
+            radius: 8,
+            backgroundColor: MomenticaColor.main,
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "비밀번호 변경",
+                  style: MomenticaTextStyle.button1(
+                    color: MomenticaColor.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              MomenticaTextField(
+                controller: previousController,
+                focusNode: previousFocusNode,
+                caption: "이전 비밀번호",
+                type: SuffixType.password,
+              ),
+              const SizedBox(height: 32),
+              MomenticaTextField(
+                controller: nextController,
+                focusNode: nextFocusNode,
+                caption: "새 비밀번호",
+                type: SuffixType.password,
+              ),
+              const SizedBox(height: 32),
+              MomenticaTextField(
+                controller: confirmController,
+                focusNode: confirmFocusNode,
+                caption: "새 비밀번호 확인",
+                type: SuffixType.password,
+              ),
+              const SizedBox(height: 36),
+              Text(
+                "비밀번호 찾기",
+                style: MomenticaTextStyle.body1(
+                  color: MomenticaColor.systemGray600,
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
