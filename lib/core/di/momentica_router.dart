ee9cafa5aab0880c5cd5fc3177637fb63eae9d43
유지우 <treeyu12345@naver.com>
2024-12-07 01:40:15 +0900

    TRUCK :: 디렉토리 경로 변경

--- lib/core/di/momentica_router.dart
+++ lib/core/di/momentica_router.dart
@@ -3,7 +3,7 @@ import 'package:momentica/presentation/main/view/main_screen.dart';
 import 'package:momentica/presentation/my/view/address/my_address_edit_screen.dart';
 import 'package:momentica/presentation/my/view/address/my_address_screen.dart';
 import 'package:momentica/presentation/my/view/counselor/my_counselor_screen.dart';
-import 'package:momentica/presentation/my/view/my_order_screen.dart';
+import 'package:momentica/presentation/my/view/order/my_order_screen.dart';
 import 'package:momentica/presentation/my/view/question/my_question_screen.dart';
 import 'package:momentica/presentation/my/view/my_screen.dart';
 import 'package:momentica/presentation/my/view/my_terms_screen.dart';
