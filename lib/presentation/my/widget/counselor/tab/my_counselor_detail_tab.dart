2cf79d5a82218a9aba5ad8f62e4894481d19b6dc
유지우 <treeyu12345@naver.com>
2024-12-07 01:38:22 +0900

    FEAT :: 고객센터 페이지의 문의 디테일 탭 퍼블리싱

--- lib/presentation/my/widget/counselor/tab/my_counselor_detail_tab.dart
+++ lib/presentation/my/widget/counselor/tab/my_counselor_detail_tab.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:momentica/core/di/momentica_style.dart';
 
 class MyCounselorDetailTab extends StatelessWidget {
   const MyCounselorDetailTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        child: ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.only(bottom: index == 10 - 1 ? 0 : 12),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    width: 1,
+                    color: MomenticaColor.systemGray100,
+                    strokeAlign: BorderSide.strokeAlignInside,
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "2024. 10. 14",
+                        style: MomenticaTextStyle.body2(
+                          color: MomenticaColor.main,
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Text(
+                        "카진호~",
+                        style: MomenticaTextStyle.caption3(
+                          color: MomenticaColor.systemGray800,
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                                maxCrossAxisExtent: 48,
+                                crossAxisSpacing: 8,
+                                mainAxisSpacing: 8,
+                                childAspectRatio: 1),
+                        itemCount: 12,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              color: MomenticaColor.systemGray300,
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
