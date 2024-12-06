cd6b7bf019f6a078b5221a34078dbfa895ae73d7
유지우 <treeyu12345@naver.com>
2024-12-07 01:38:49 +0900

    FEAT :: 고객센터 페이지의 문의 탭 퍼블리싱

--- lib/presentation/my/widget/counselor/tab/my_counselor_inquiry_tab.dart
+++ lib/presentation/my/widget/counselor/tab/my_counselor_inquiry_tab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:momentica/core/component/momentica_button.dart';
 import 'package:momentica/core/component/momentica_text_field.dart';
+import 'package:momentica/core/di/momentica_style.dart';
 import 'package:momentica/core/type/suffix_type.dart';
 
 class MyCounselorInquiryTab extends StatefulWidget {
@@ -46,6 +49,47 @@ class _MyCounselorInquiryTabState extends State<MyCounselorInquiryTab> {
             caption: "문의 내용",
             hintText: "문의하실 내용을 500자 내로 작성해주세요.",
           ),
+          const SizedBox(height: 40),
+          Text(
+            "참고 사진",
+            style: MomenticaTextStyle.label1(
+              color: MomenticaColor.black,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: MomenticaColor.systemGray50,
+              borderRadius: BorderRadius.circular(4),
+              border: Border.all(
+                color: MomenticaColor.systemGray300,
+                width: 1,
+                strokeAlign: BorderSide.strokeAlignInside,
+              ),
+            ),
+            child: SvgPicture.asset(
+              "$iconCoreAsset/outlined_gallery_icon.svg",
+              width: 28,
+              height: 28,
+            ),
+          ),
+          const Spacer(),
+          MomenticaButton(
+            event: () {},
+            backgroundColor: MomenticaColor.systemGray300,
+            content: Text(
+              "문의하기",
+              style: MomenticaTextStyle.button1(
+                color: MomenticaColor.white,
+              ),
+            ),
+            height: 52,
+            radius: 8,
+          ),
+          const SizedBox(height: 12),
         ],
       ),
     );
