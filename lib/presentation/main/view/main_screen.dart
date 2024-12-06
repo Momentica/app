837e5a46f6657167e1adc98338748c1ac56c2f2c
유지우 <treeyu12345@naver.com>
2024-12-07 01:37:03 +0900

    FEAT :: Main 페이지 AppBar 구현

--- lib/presentation/main/view/main_screen.dart
+++ lib/presentation/main/view/main_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:momentica/core/component/momentica_app_bar.dart';
+import 'package:momentica/core/component/momentica_gesture.dart';
+import 'package:momentica/core/di/momentica_style.dart';
 import 'package:momentica/core/layout/momentica_layout.dart';
+import 'package:momentica/presentation/main/widget/app_bar/main_app_bar_my_toggle.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -7,7 +13,33 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MomenticaLayout(
-      child: Column(
+      appBar: MomenticaAppBar(
+        height: 58,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 24),
+              child: Image.asset(
+                "$imageCoreAsset/momentica_logo_image.png",
+                width: 140,
+                height: 24,
+              ),
+            ),
+            Row(
+              children: [
+                SvgPicture.asset("$iconCoreAsset/add_icon.svg"),
+                const SizedBox(width: 20),
+                MomenticaGesture(
+                  event: () => context.push("/my"),
+                  child: const MainAppBarMyToggle(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      child: const Column(
         children: [],
       ),
     );
