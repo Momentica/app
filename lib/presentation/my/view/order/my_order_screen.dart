b471c623860f2aa1f959026e9e75b6bed9c8bdf8
유지우 <treeyu12345@naver.com>
2024-12-16 13:19:05 +0900

    FEAT :: 주문 내역 페이지 퍼블리싱

--- lib/presentation/my/view/order/my_order_screen.dart
+++ lib/presentation/my/view/order/my_order_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:momentica/core/component/momentica_app_bar.dart';
+import 'package:momentica/core/component/momentica_gesture.dart';
+import 'package:momentica/core/di/momentica_style.dart';
+import 'package:momentica/core/layout/momentica_layout.dart';
+import 'package:momentica/presentation/my/widget/order/my_order_item_widget.dart';
 
 class MyOrderScreen extends StatelessWidget {
   const MyOrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MomenticaLayout(
+      appBar: MomenticaAppBar(
+        height: 56,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 4),
+          child: Row(
+            children: [
+              MomenticaGesture(
+                event: () => context.pop(),
+                child: SvgPicture.asset("$iconCoreAsset/left_arrow_icon.svg"),
+              ),
+              const SizedBox(width: 8),
+              Text(
+                "주문 내역",
+                style: MomenticaTextStyle.caption1(
+                  color: MomenticaColor.black,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(bottom: index == 5 - 1 ? 0 : 8),
+                child: const MyOrderItemWidget(),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
